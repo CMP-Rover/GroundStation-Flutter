b48abd7a06337a86eb2580f235f167accc021c16
Yousef Gamal <yousef.gamal.it@gmail.com>
2021-04-30 13:42:41 +0200

    sensors socket v1

---
+++ serverSide/client.js
@@ -0,0 +1,27 @@
+const WebSocket = require('ws');
+const socket = new WebSocket('ws://localhost:3000');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    console.log('Connected to WS Server')
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+const sendMessage = (message) => {
+    socket.send(message);
+}
+
+const port = 8000;
+const express = require('express');
+const app = express();
+
+app.listen(port, () => {console.log("Start Listening");})
+app.get("/", (req, res) => {
+    mess = req.query.message;
+    sendMessage(mess);
+    res.send("Message is sent");
+});
