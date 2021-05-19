7edbd264af4158241cc6000d683b016a8e21f6c4
YousefGamal <yousef.gamal.it@gmail.com>
2021-05-19 04:09:16 +0200

    Movement Socket Test

--- front-end/cmp_rover/lib/demo_widgets/ControlPadDemo.dart
+++ front-end/cmp_rover/lib/demo_widgets/ControlPadDemo.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:control_pad/control_pad.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
 class ControlPadDemo extends StatefulWidget {
   @override
@@ -7,6 +9,15 @@ class ControlPadDemo extends StatefulWidget {
 }
 
 class _ControlPadDemoState extends State<ControlPadDemo> {
+  final movement =
+      IOWebSocketChannel.connect("wss://cmp-rover.herokuapp.com/movement");
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    movement.sink.close(status.goingAway);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,11 +26,25 @@ class _ControlPadDemoState extends State<ControlPadDemo> {
           title: Text("Demo Controller"),
         ),
         body: Center(
-          child: JoystickView(
-            onDirectionChanged: (degrees, distance) {
-              print("Degree = $degrees, and Distance = $distance");
-            },
-          ),
+          child: StreamBuilder(
+              stream: movement.stream,
+              builder: (context, snapshot) {
+                return Column(
+                  children: [
+                    JoystickView(
+                      backgroundColor: Colors.indigo[800],
+                      innerCircleColor: Colors.indigo[600],
+                      interval: Duration(seconds: 1),
+                      showArrows: false,
+                      onDirectionChanged: (degrees, distance) {
+                        print("Degree = $degrees, and Distance = $distance");
+                        if (degrees != 0) movement.sink.add("$degrees");
+                      },
+                    ),
+                    snapshot.hasData ? Text("${snapshot.data}") : Container()
+                  ],
+                );
+              }),
         ),
       ),
     );
